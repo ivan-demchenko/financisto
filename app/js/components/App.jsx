87fa5ad10675a31a3a6659e7597ceae6c7491e8a
Ivan Demchenko <raqy.style@gmail.com>
2017-09-14 20:26:16 +1000

    Better layout

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -3,10 +3,12 @@ import { Component } from 'react';
 
 export default class App extends Component {
     render() {
-        return (<div className="container">
-            <header>this is a header</header>
-            <main>main content</main>
-            <footer>this is a footer</footer>
-        </div>);
+        return (
+            <div className="container">
+                <header className="header">Financier</header>
+                <main className="main-content">main content</main>
+                <footer className="footer">Copyright &copy; Ivan Demchenko</footer>
+            </div>
+        );
     }
 }
