77b6e1f0090a3a6c2916ba1796bdc719232f1b0c
Ivan Demchenko <raqy.style@gmail.com>
2017-09-14 21:44:21 +1000

    component that sends data to backend

---
+++ app/js/components/CSVInput.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import { merge } from 'ramda';
+
+const editCSV = (code) => ({
+    type: 'EDIT_CSV',
+    code
+});
+
+const postCSV = (code) => ({
+    type: 'POST_CSV',
+    code
+});
+
+const receivedFeedback = (data) => ({
+    type: 'POST_CSV_SUCCESS',
+    data
+});
+
+const initial = {
+    code: ''
+};
+
+export const sendDataEpic = (action$) =>
+    action$.ofType('POST_CSV')
+        .flatMap(action => 
+            fromPromise(fetch('/api/csv', { method: 'POST', body: JSON.stringify({
+                csv: action.code
+            })}))
+        )
+        .do(resp => console.log(resp))
+        .map(receivedFeedback)
+
+export const reducer = (state = initial, action) => {
+    switch(action.type) {
+        case 'EDIT_CSV':
+            return merge(state, {code: action.code});
+
+        case 'POST_CSV':
+            return state;
+
+        case 'POST_CSV_SUCCESS':
+            return state;
+
+        default:
+            return state;
+
+    }
+};
+
+const view = (state) =>
+    <div>
+        <textarea onChange={(e) => state.editCSV(e.target.value)} value={state.code}></textarea>
+        <button onClick={() => state.postCSV(state.code)}>Do it!</button>
+    </div>;
+
+export default connect(
+    (state) => state.csvInput,
+    { editCSV, postCSV, receivedFeedback }
+)(view);
