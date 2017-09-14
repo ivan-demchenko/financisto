c5decc6607af87b7d2a82449417e2f4ebc3a7ba4
Ivan Demchenko <raqy.style@gmail.com>
2017-09-14 21:43:47 +1000

    set up middleware

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -1,16 +1,33 @@
 import 'rxjs';
 import * as React from 'react';
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import { Component } from 'react';
 
+import CSVInput, { reducer as CSVInputReducer, sendDataEpic } from './CSVInput';
+
+const epicMiddleware = createEpicMiddleware(sendDataEpic);
+const rootReducer = combineReducers({
+    csvInput: CSVInputReducer
+});
+const rootStore = createStore(
+    rootReducer,
+    applyMiddleware(epicMiddleware)
+);
 
 export default class App extends Component {
     render() {
         return (
+            <Provider store={rootStore}>
             <div className="container">
                 <header className="header">Financier</header>
-                <main className="main-content">main content</main>
+                <main className="main-content">
+                    <CSVInput />
+                </main>
                 <footer className="footer">Copyright &copy; Ivan Demchenko</footer>
             </div>
+            </Provider>
         );
     }
 }
