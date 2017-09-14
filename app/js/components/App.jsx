fd254e9763295070b8ea0b9b6daf49ae349642ac
Ivan Demchenko <raqy.style@gmail.com>
2017-09-14 19:55:39 +1000

    Added layout to the viewport

--- app/js/components/App.jsx
+++ app/js/components/App.jsx
@@ -3,7 +3,10 @@ import { Component } from 'react';
 
 export default class App extends Component {
     render() {
-        let time = new Date().toLocaleTimeString();
-        return <span>{ time }</span>;
+        return (<div className="container">
+            <header>this is a header</header>
+            <main>main content</main>
+            <footer>this is a footer</footer>
+        </div>);
     }
 }
