d8a8a3cb9ebe29c567e42491013294347a92bc12
Ivan Demchenko <raqy.style@gmail.com>
2017-09-18 08:10:01 +1000

    Better display of grouped data

--- app/js/components/GroupedData.jsx
+++ app/js/components/GroupedData.jsx
@@ -23,9 +23,27 @@ export const reducer = (state = initial, action) => {
     }
 };
 
+const renderCategories = (month, cats) => r.compose(
+    r.map(([cat, val]) =>
+    <div className="gd-category" key={'c' + cat}>
+        <a href="#" className="gd-category__name">{cat}</a>
+        <div className="gd-category__val">{val}</div>
+    </div>),
+    r.toPairs
+)(cats);
+
+const renderMonths = r.compose(
+    r.map(([month, cats]) =>
+    <div className='gd-month' key={'m' + month}>
+        <strong className='gd-month__label'>{month}</strong>,
+        <div className="gd-categories">{renderCategories(month, cats)}</div>
+    </div>),
+    r.toPairs
+);
+
 const view = (state) =>
     <div className="grouped-data">
-        <pre>{JSON.stringify(state.data.grouped, null, 2)}</pre>
+        {renderMonths(state.data.grouped)}
     </div>;
 
 export default connect(
