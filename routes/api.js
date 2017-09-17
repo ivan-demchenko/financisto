d46d5a4b3c6ae4dc2db9e37459c2d82ca0022bd0
Ivan Demchenko <raqy.style@gmail.com>
2017-09-17 12:31:11 +1000

    Set up bpy parser for api post requests

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({ type: 'application/json'});
 var transformer = require('../service/main');
 
-router.post('/csv', (req, res, next) => {
-  console.log(req.body);
+router.post('/csv', jsonParser, (req, res, next) => {
   res.json(transformer(req.body.csv));
 });
 
