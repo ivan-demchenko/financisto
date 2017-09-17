292dd406fc175d23e020e80bb5ff28c647815004
Ivan Demchenko <raqy.style@gmail.com>
2017-09-17 12:31:48 +1000

    Improved CSV parser

--- service/main.js
+++ service/main.js
@@ -4,10 +4,15 @@ const guessCategory = require('./categoriser');
 
 const parseCSV = r.compose(
     r.converge(r.map, [
-        r.compose(r.zipObj, r.split(','), r.head),
-        r.compose(r.split(','), r.tail)
+        r.compose(
+            r.zipObj, r.split(','), r.init, r.head
+        ),
+        r.compose(
+            r.map(r.compose(r.map(r.replace(/\"/gi, '')), r.split(','), r.init)), r.tail
+        )
     ]),
-    r.split('\n')
+    r.split('\n'),
+    r.trim
 )
 
 module.exports = r.pipe(
