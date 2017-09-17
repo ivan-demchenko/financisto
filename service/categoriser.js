c458b2423ee182166d370f31d5e20527ff5abda9
Ivan Demchenko <raqy.style@gmail.com>
2017-09-17 14:19:23 +1000

    Better categoriser

--- service/categoriser.js
+++ service/categoriser.js
@@ -4,32 +4,35 @@ module.exports = r.cond([
     [ r.test(/PARKING/ig)
     , r.always('Parking')
     ],
-    [ r.test(/COLES|BAKERSDELIGHT|CELLARBRATIONS|LIQUORLAND|V\/CELLARS|The Junction Coffee/ig)
+    [ r.test(/COLES|BAKERSDELIGHT|CELLARBRATIONS|SULU 33|GROCER|LIQUORLAND|V\/CELLARS|The Junction Coffee/ig)
     , r.always('Food and drinks')
     ],
-    [ r.test(/THE MORRISON BAR|SYDNEY TOWER EYE|CAFFE|Cafe|Scenic World Katoomba|ROYAL COPENHAGEN/ig)
+    [ r.test(/THE MORRISON BAR|SYDNEY TOWER EYE|ICE CREAM|Mecca Espresso|BAR|PATISS|PATISSERIES|CAFFE|Cafe|Scenic World Katoomba|ROYAL COPENHAGEN/ig)
     , r.always('Having fun')
     ],
-    [ r.test(/H AND M/ig)
+    [ r.test(/H AND M|TK MAXX/ig)
     , r.always('Clothings')
     ],
-    [ r.test(/Burns Bay Medical|NATIONAL HOME DOCTOR|RNS HOSP EMERGENCY|Pharm/ig)
+    [ r.test(/Burns Bay Medical|iHerb|LAVERTY|NATIONAL HOME DOCTOR|RNS HOSP EMERGENCY|Pharm/ig)
     , r.always('Medical/Pharmacy')
     ],
-    [ r.test(/CHEMIST|TARGET|IKEA|Kidstuff|GOODGUYS/ig)
+    [ r.test(/CHEMIST|TARGET|IKEA|JB HI FI|KARCHER|Kidstuff|GOODGUYS|HOT DOLLAR/ig)
     , r.always('Chemist/Home stuff')
     ],
     [ r.test(/KATHMANDU|99 BIKES/ig)
     , r.always('Sports')
     ],
-    [ r.test(/TPG Internet|AMAYSIM/ig)
+    [ r.test(/TPG Internet|AMAYSIM|OPTUS/ig)
     , r.always('Internet/Mobile')
     ],
-    [ r.test(/OPAL|ETOLL|CABS/ig)
+    [ r.test(/OPAL|ETOLL|WHARF|CABS|NEWS EXPRESS PARRAMATTA/ig)
     , r.always('Transport')
     ],
     [ r.test(/atlassian/ig)
     , r.always('Income')
+    ],
+    [ r.test(/TFR Westpac eSa/ig)
+    , r.always('Savings')
     ],
     [ r.test(/PYMT RAINE AND|LOCKSMITHS/ig)
     , r.always('Rent')
