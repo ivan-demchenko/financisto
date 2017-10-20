63816d61a410fb3ed37d4e78da085acc29d03126
Ivan Demchenko <raqy.style@gmail.com>
2017-10-20 22:09:39 +1100

    Cleaned up the setup, fixed api setup

--- routes/api.js
+++ routes/api.js
@@ -4,20 +4,22 @@ var bodyParser = require('body-parser');
 var debug = require('debug')('financier:api');
 var jsonParser = bodyParser.json({ type: 'application/json'});
 var { groupByMonthAndCategory, returnUnknownsOnly } = require('../service/main');
-var MongoClient = require('mongodb').MongoClient;
+var Reader = require('fantasy-readers');
 
 router.post('/csv', jsonParser, (req, res, next) => {
 
-  var url = 'mongodb://mongodb:27017/financier';
-  MongoClient.connect(url, function(err, db) {
-    debug("Connected correctly to server.");
-    db.close();
-  });
+  debug('received some data to /csv');
 
   res.json({
-    grouped: groupByMonthAndCategory(req.body.csv),
-    unknowns: returnUnknownsOnly(req.body.csv)
+    grouped: {}, //groupByMonthAndCategory(req.body.csv),
+    unknowns: {} //returnUnknownsOnly(req.body.csv)
   });
+
 });
+
+const saveData = (data) =>
+  new Reader(env => {
+    env.db.insert(data)
+  })
 
 module.exports = router;
