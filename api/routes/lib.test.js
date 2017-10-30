b331efc1128657c4b09f8e4f739df4687e86ad5d
Ivan Demchenko <raqy.style@gmail.com>
2017-10-31 08:04:00 +1100

    Better tests

--- api/routes/lib.test.js
+++ api/routes/lib.test.js
@@ -8,7 +8,8 @@ describe('Logic for /api/csv', () => {
 
   beforeEach(() => {
     mockedModel = {
-      find: () => Promise.resolve([{a: 1}, {a: 2}]),
+      find: jest.fn(), //() => Promise.resolve([{a: 1}, {a: 2}]),
+      save: jest.fn(),
       update: jest.fn()
     };
   });
@@ -17,11 +18,12 @@ describe('Logic for /api/csv', () => {
     mockedModel = null;
   });
 
-  describe('empty request body or missing info in request body', () => {
+  describe('ideal scenario', () => {
 
-    it('should merge with the previous data', () => {
+    it('should merge parsed CSV data with the existing data', () => {
       const failureFn = jest.fn();
       const mockedCSV = 'q,w\n1,2\n4,5'
+      mockedModel.find.mockReturnValueOnce(Promise.resolve([{a: 1}, {a: 2}]))
       appendNewRecords
         .run({
           model: mockedModel,
@@ -38,8 +40,13 @@ describe('Logic for /api/csv', () => {
       expect(failureFn.mock.calls.length).toBe(0);
     });
 
+  });
+
+  describe('payload is corrupted', () => {
+
     it('should fail if the request body is empty', () => {
       const successFn = jest.fn();
+      mockedModel.find.mockReturnValueOnce(Promise.resolve([{a: 1}, {a: 2}]))
       appendNewRecords
         .run({ model: mockedModel, requestBody: {} })
         .fork(
@@ -54,11 +61,49 @@ describe('Logic for /api/csv', () => {
 
     it('should fail if `csv` is empty within request body', () => {
       const successFn = jest.fn();
+      mockedModel.find.mockReturnValueOnce(Promise.resolve([{a: 1}, {a: 2}]))
       appendNewRecords
         .run({ model: mockedModel, requestBody: { csv: '' } })
         .fork(
           err => {
             expect(err).toBe(Msg.api.csv.missingData);
+            expect(mockedModel.update.mock.calls.length).toBe(0);
+          },
+          successFn
+        );
+      expect(successFn.mock.calls.length).toBe(0);
+    });
+
+  });
+
+  describe('model is cheeky', () => {
+
+    it('should fail if the read from model is unsuccessful', () => {
+      const failedMsg = new Error('Failed to read from model');
+      const successFn = jest.fn();
+      mockedModel.find.mockReturnValueOnce(Promise.reject(failedMsg))
+      appendNewRecords
+        .run({ model: mockedModel, requestBody: { csv: 'a,b\n1,2' } })
+        .fork(
+          err => {
+            expect(err).toBe(failedMsg.message);
+            expect(mockedModel.update.mock.calls.length).toBe(0);
+          },
+          successFn
+        );
+      expect(successFn.mock.calls.length).toBe(0);
+    });
+
+    it('should fail if saving the model was unsuccessful but read was good', () => {
+      const failedMsg = new Error('Failed to save model');
+      const successFn = jest.fn();
+      mockedModel.find.mockReturnValueOnce(Promise.resolve([{a: 1}]));
+      mockedModel.save.mockReturnValueOnce(Promise.reject(failedMsg));
+      appendNewRecords
+        .run({ model: mockedModel, requestBody: { csv: 'a,b\n1,2' } })
+        .fork(
+          err => {
+            expect(err).toBe(failedMsg.message);
             expect(mockedModel.update.mock.calls.length).toBe(0);
           },
           successFn
